8a63b2e9dc3372ed60ff52a12252c5cc71568652
MilosMIklja <mikljamilos@gmail.com>
2023-08-14 21:19:34 +0200

    Commit

---
+++ src/test/java/tests/FailedLoginTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import dataGenerator.DataProviders;
+import methods.LoginMethods;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class FailedLoginTest extends BaseTest{
+    @Test(dataProvider = "FailedLoginDataSet", dataProviderClass = DataProviders.class)
+    public void failedLogin(String username, String password, String error){
+        LoginPage loginPage = new LoginPage(driver);
+        LoginMethods loginMethods = new LoginMethods(driver);
+        loginMethods.performLogin(username, password);
+
+        loginPage.verifyFailedLogin(error);
+    }
+
+}
